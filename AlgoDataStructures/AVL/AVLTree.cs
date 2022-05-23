18e5be21beb21bd1754b798a68d8ea54e336051c
Collin Kite <me@collinkite.com>
2022-05-22 19:45:06 -0600

    Working on Breadth First ToArray, then need to Setup Balancing

---
+++ AlgoDataStructures/AVL/AVLTree.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDataStructures.AVL
+{
+    public class AVLTree<T> where T : IComparable<T>
+    {
+        public AVLTreeNode<T> Root { get; set; }
+
+        public int Count { get; set; } = 0;
+
+        public void Add(T val)
+        {
+            if (Root == null)
+            {
+                Root = new AVLTreeNode<T>(val);
+                Root.Count++;
+            }
+            else
+            {
+                Root.Add(val);
+            }
+            Count++;
+        }
+
+        public void Clear()
+        {
+            Root = null;
+            Count = 0;
+        }
+
+        public int Height()
+        {
+            return Root != null ? Root.Height() : 0;
+        }
+
+        public void Remove(T val)
+        {
+            if (Root != null && Contains(val))
+            {
+                Root = Root.Remove(val);
+                Count--;
+            }
+        }
+
+        public bool Contains(T val)
+        {
+            if(Root != null)
+            {
+                return Root.Contains(val);
+            }
+            return false;
+        }
+
+        public string InOrder()
+        {
+            if (Root != null)
+            {
+                return Root.InOrder();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public T[] ToArray()
+        {
+            if(Root != null)
+            {
+                for (int i = 0; i < Root.Height(); i++)
+                {
+                    Root.ToArray(i);
+                }
+            }
+            else
+            {
+                return
+            }
+        }
+
+        public string PostOrder()
+        {
+            if (Root != null)
+            {
+                return Root.PostOrder();
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+
+        public string PreOrder()
+        {
+            if (Root != null)
+            {
+                return Root.PreOrder();
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+    }
+}
+
+   
---
+++ AlgoDataStructures/AVL/AVLTreeNode.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDataStructures.AVL
+{
+    public class AVLTreeNode<T> where T : IComparable<T>
+    {
+
+        public AVLTreeNode(){}
+        public AVLTreeNode(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public AVLTreeNode<T> Left { get; set; }
+        public AVLTreeNode<T> Right { get; set; }
+
+        public void Add(T val)
+        {
+            if(Data.CompareTo(val) > 0) //if the value we're adding is less than the current nodes value
+            {
+                if(Left != null)
+                {
+                    Left.Add(val);
+                }
+                else
+                {
+                    Left = new AVLTreeNode<T>();
+                    Left.Data = val;
+                    Left.Count++;
+                }
+            }
+            else if(Data.CompareTo(val) < 0) //if the value we're adding is greater than the current nodes value
+            {
+                if (Right != null) //If we have further child nodes keep traversing till we hit null or same value
+                {
+                    Right.Add(val);
+                }
+                else //Otherwise we're at the end and we need to add out value
+                {
+                    Right = new AVLTreeNode<T>();
+                    Right.Data = val;
+                    Right.Count++;
+                }
+            }
+            else //value is the same as the current node and we need to increase the count
+            {
+                Count++;
+            }
+        }
+
+        public int Count { get; set; } = 0; //for duplicate values we just increase/decrease the count of how many values exist in the node
+
+        public int Height()
+        {
+            int LeftSize = 0;
+            int RightSize = 0;
+            if(Left != null)
+            {
+                LeftSize += Left.Height();
+            }
+            else if(Right != null)
+            {
+                RightSize += Right.Height();
+            }
+            if(LeftSize >= RightSize) //made sure to add check if they're the same
+            {
+                return LeftSize + 1; // add 1 to include root
+            }
+            else
+            {
+                return RightSize + 1;
+            }
+        }
+
+        public AVLTreeNode<T> Remove(T val, int count = 1)
+        {
+            if (val.CompareTo(Data) < 0) //val is less than the data
+            {
+                Left = Left.Remove(val, count);
+                return this;
+            }
+            else if (val.CompareTo(Data) > 0) //Val is greater than the data
+            {
+                Right = Right.Remove(val, count);
+                return this;
+            }
+            //Reached our value 
+                if (Count > count)
+                {
+                    Count =  Count - count;
+                    return this;
+                }
+                else
+                {
+                    //if the node we're deleting has only one child or no children
+                    if (Left == null)
+                    {
+                        return Right;
+                    }
+                    else if (Right == null)
+                    {
+                        return Left;
+                    }
+                    //If the node we're deleting has 2 children - find the  highest on the left side and copy it to our value we're deleting (don't forget count) and then delete the original copy
+                    else
+                    {
+                        (T, int) DataToCopy = FindLargestOnLeft(Left);
+                        Data = DataToCopy.Item1;
+                        Count = DataToCopy.Item2;
+                        Left = Left.Remove(Data, Count);
+                        return this;
+                    }
+                }
+            
+        }
+
+        public (T, int) FindLargestOnLeft(AVLTreeNode<T> node) //Return touple because we have to copy up our count too
+        {
+            if(node.Right != null)
+            {
+                return FindLargestOnLeft(node.Right);
+            }
+            return (node.Data, node.Count);
+        }
+
+        public string InOrder()
+        {
+            string order = "";
+            order = InOrder(order);
+            order = order.TrimEnd();
+            order = order.TrimEnd(',');
+            return order;
+        }
+        public string InOrder(string Order)
+        {
+            if(Left != null)
+            {
+                Order = Left.InOrder(Order);
+            }
+            for (int i = 0; i < Count; i++)
+            {
+                Order += Data.ToString() + ", ";
+            }
+            if(Right != null)
+            {
+                Order = Right.InOrder(Order);
+            }
+            return Order;
+
+        }
+
+
+        public string PreOrder()
+        {
+            string order = "";
+            order = PreOrder(order);
+            order = order.TrimEnd();
+            order = order.TrimEnd(',');
+            return order;
+        }
+
+        public string PreOrder(string order)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                order += Data.ToString() + ", ";
+            }
+            if(Left != null)
+            {
+                order = Left.PreOrder(order);
+            }
+            if(Right != null)
+            {
+                order = Right.PreOrder(order);
+            }
+            return order;
+
+        }
+
+        public string PostOrder()
+        {
+            string order = "";
+            order = PostOrder(order);
+            order = order.TrimEnd();
+            order = order.TrimEnd(',');
+            return order;
+        }
+        public string PostOrder(string order)
+        {
+            if (Left != null)
+            {
+                order = Left.PostOrder(order);
+            }
+            if (Right != null)
+            {
+                order = Right.PostOrder(order);
+            }
+            for (int i = 0; i < Count; i++)
+            {
+                order += Data.ToString() + ", ";
+            }
+            return order;
+
+        }
+
+        public T[] ToArray(int level, T[] Arr, int index)
+        {
+            //breadth First - print by levels - Left, Root, Right
+            // ex.
+            /*
+             
+            [11]
+            /  \
+          [10][24]
+              /  \
+            [13][56]
+
+            would return: 11, 10, 24, 13, 56
+                
+             */
+
+            if(level == 0)
+            {
+                Arr[index] = Data;
+                index++;
+            }
+            else
+            {
+                Left.ToArray((level - 1), Arr, index);
+                Right.ToArray((level - 1), Arr, index);
+            }
+            return Arr;
+        }
+
+        public (T[], int) ToArray(T[] Arr, int index)
+        {
+            if (Left != null) //Traverse to left most node
+            {
+                (T[], int) DataStuff = Left.ToArray(Arr, index);
+                Arr = DataStuff.Item1;
+                index = DataStuff.Item2;
+            }
+            for (int i = 0; i < Count; i++)
+            {
+                Arr[index] = Data;
+                index++;
+            }
+            if(Right != null)
+            {
+                (T[], int) DataStuff = Right.ToArray(Arr, index);
+                Arr = DataStuff.Item1;
+                index = DataStuff.Item2;
+            }
+            return (Arr, index);
+   
+
+        }
+        public bool Contains(T value)
+        {
+            if (Data.Equals(value))
+            {
+                return true;
+            }
+            else if (value.CompareTo(Data) > 0 && Right != null)  //Greater than current node and we can keep traversing
+            {
+                return Right.Contains(value);
+            }
+            else if (value.CompareTo(Data) < 0 && Left != null) //Less than current node and we can keep traversing
+            {
+                return Left.Contains(value);
+            }
+            return false;
+        }
+    }
+}
