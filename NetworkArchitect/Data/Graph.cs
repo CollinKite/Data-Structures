9431d6df058b4a15be74959f10e3c9a61f1220c1
Collin Kite <me@collinkite.com>
2022-06-02 01:29:38 -0600

    Started Setting Up Graph

--- NetworkArchitect/Data/Graph.cs
+++ NetworkArchitect/Data/Graph.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NetworkArchitect.Data
@@ -12,8 +13,32 @@ namespace NetworkArchitect.Data
 
         public Graph(string[] setupLines)
         {
-            Nodes = new();
-            //setup graph
+            //AX1,AX2,AX3,AX4,AX5
+            //Add Nodes
+            string[] Nodes = setupLines[0].Split(',');
+            for (int i = 0; i < Nodes.Length; i++)
+            {
+                Node node = new(Nodes[i]);
+                this.Nodes.Add(node);
+            }
+            //Add Edges
+            //AX1,AX4: 3,AX2: 3,AX3: 6
+            //AX2,AX1: 3,AX3: 3,AX4: 6
+            //AX3,AX2: 3,AX1: 6,AX4: 41
+            //AX4,AX1: 3,AX2: 6,AX3: 4,AX5: 15
+            //AX5,AX4: 15
+            for (int j = 1; j < setupLines.Length; j++)
+            {
+                string[] currentEdge = setupLines[j].Split(',');
+                for (int Edge = 1; Edge < currentEdge.Length; Edge++)
+                {
+                    string StartId = currentEdge[0];
+                    string EndId = currentEdge[Edge];
+                    int weight;
+                    Regex rx = new Regex(":[0-9]{1,3}", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                    MatchCollection matches = rx.Matches(EndId);
+                }
+            }
         }
 
         public Node FindNode(string id)
